693c680fc1c0016fd673f270d07125a147479313
Ashay Parikh <ashayparikh22@gmail.com>
2022-08-12 13:17:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# KontestHub
+It’s simple: an online, contest-hosting platform that MAXIMIZES student collaboration and freedom while MINIMIZING teacher work!
+
+## Next Update
+In a few months, we will be adding support for MATH contests. We hope to allow students to use this platform for in-classroom review before tests/quizzes, competitive math contests, and much more!
+
+
+### Developers:
+Ashay Parikh and Labdhi Jain
--- client/src/Pages/Particle.tsx
+++ client/src/Pages/Particle.tsx
@@ -1,5 +1,7 @@
 import Particles from "react-tsparticles";
-import React from 'react'; // importing FunctionComponent
+import { loadFull } from "tsparticles";
+import React from 'react';
+
 
 const options : any = {
   "fullScreen": {
@@ -147,14 +149,23 @@ const options : any = {
   }
 }
 
-const CustomParticles = () => {
+const App = () => {
+  const particlesInit = async (main : any) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(main);
+  };
+
   return (
     <Particles
-      className="particles"
+      id="tsparticles"
+      init={particlesInit}
       options={options}
-      style={{background: `#073b4c`}}
     />
   );
 };
 
-export default CustomParticles;
+export default App;
